98f4679e1f03e31e3b3ecb5c732f8027ec2bca7f
Dano <danielaherniv@gmail.com>
2019-11-17 11:37:25 -0500

    added css

--- googgle-books-search-v2/src/App.js
+++ googgle-books-search-v2/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Header from './Header';
 import logo from './logo.svg';
 import './App.css';
 
---
+++ googgle-books-search-v2/src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import App from './App';
+
+const Header = () => {
+    return (
+        <header>
+            <br />
+            <br />
+            <i class="fas fa-book fa-4x"></i>
+            <h1>Google Book Search</h1>
+        </header>
+    )
+}
+
+export default Header;
