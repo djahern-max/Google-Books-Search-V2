21e0509575c79361e220d921208ac1edf146cac4
Dane Ahern <danielaherniv@gmail.com>
2019-11-18 14:51:22 -0500

    updated card for link and description

--- src/BookCard.js
+++ src/BookCard.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import App from './App';
 
-const BookCard = (props) => {
-    return (
-
-        <div className="card-container">
-
-            <img src={props.image} alt="" />
-            <div className="desc">
-                <h2>{props.title}</h2>
-                <p>Author: {props.author}</p>
-                <p>Published: {props.published === '0000' ? 'Not Available' : props.published.substring(0, 4)}</p>
-            </div>
-        </div>
-
-
-    )
-}
+const BookCard = props => {
+  return (
+    <div className='card-container'>
+      <img src={props.image} alt='' />
+      <div className='desc'>
+        <h2>{props.title}</h2>
+        <p>AUTHOR: {props.author}</p>
+        <p>
+          PUBLISHED:{' '}
+          {props.published === '0000'
+            ? 'Not Available'
+            : props.published.substring(0, 4)}
+        </p>
+        <p>LINK: {props.link}</p>
+        <p>DESCRIPTION: {props.description}</p>
+      </div>
+    </div>
+  );
+};
 
 export default BookCard;
